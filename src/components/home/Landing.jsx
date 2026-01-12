3e28f1e8ec10e5da848d1980f964294f70995b9b
mmcmullinyohe <mmcmullinyohe@gmail.com>
2026-01-12 12:04:40 -0600

    landing animations

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -9,33 +9,55 @@ const Landing = () => {
       id="section-hero"
       aria-label="section"
       className="no-top no-bottom vh-100"
-      data-bgimage="url(images/bg-shape-1.jpg) bottom"
       style={{ background: `url(${backgroundImage}) bottom / cover` }}
     >
       <div className="v-center">
         <div className="container">
           <div className="row align-items-center">
-            <div className="col-md-6">
+            <div
+              className="col-md-6"
+              data-aos="fade-right"
+              data-aos-delay="100"
+            >
               <div className="spacer-single"></div>
-              <h6>
+
+              <h6 data-aos="fade-down" data-aos-delay="200">
                 <span className="text-uppercase id-color-2">
                   Ultraverse Market
                 </span>
               </h6>
+
               <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
+
+              <h1 data-aos="fade-up" data-aos-delay="300">
+                Create, sell or collect digital items.
+              </h1>
+
+              <p className="lead" data-aos="fade-up" data-aos-delay="400">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
                 interchangeable
               </p>
+
               <div className="spacer-10"></div>
-              <Link className="btn-main lead" to="/explore">
+
+              <Link
+                className="btn-main lead"
+                to="/explore"
+                data-aos="zoom-in"
+                data-aos-delay="500"
+              >
                 Explore
               </Link>
+
               <div className="mb-sm-30"></div>
             </div>
-            <div className="col-md-6 xs-hide">
+
+            <div
+              className="col-md-6 xs-hide"
+              data-aos="fade-left"
+              data-aos-delay="300"
+            >
               <img src={NFT} className="lazy img-fluid" alt="" />
             </div>
           </div>
